71731c8e19174b7c580c47da4fb7338401296f3d
Albert Coder <eralbert9191@gmail.com>
2014-03-16 14:52:29 +0530

    formatting improved

--- circle/widget.cpp
+++ circle/widget.cpp
@@ -5,27 +5,23 @@ Widget::Widget(QWidget *parent)
      : QWidget(parent)
 {
     resize(400,500);
-
-
 }
-
-
-// Painter event to paint circle in blue colour
+/* Painter event to paint circle in blue colour */
 void Widget::paintEvent(QPaintEvent *event)
 {
 
       QPainter painter(this);
 
-      //First Circle:
-      //width&height:150px
-      //Beginning(x/y):50px/30px
-      //Each step for Angle parameter is 1/16th of a Degree
-      //so multiply Angle Value with 16
+      /**
+      First Circle:
+      width&height:150px
+      Beginning(x/y):50px/30px
+      Each step for Angle parameter is 1/16th of a Degree
+      so multiply Angle Value with 16
+      */
       painter.drawArc(50,30,150,150,0,16*360);
       painter.drawArc(198,80,100,100,0,16*360);
       painter.drawArc(294,130,50,50,0,16*360);
-
-
     };
 
 Widget::~Widget()
